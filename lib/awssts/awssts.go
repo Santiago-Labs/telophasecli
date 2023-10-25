35d2b1f60e882630f9af0df7dfaed67773afe4fc
Danny Schofield <dannyschofield28@gmail.com>
2023-10-25 13:41:17 -0400

    awssts: package for assuming role and getting env vars

---
+++ lib/awssts/awssts.go
@@ -0,0 +1,27 @@
+package awssts
+
+import "strings"
+
+func SetEnviron(currEnv []string,
+	accessKeyID,
+	secretAccessKey,
+	sessionToken string) []string {
+	var newEnv []string
+	for _, e := range currEnv {
+		if strings.Contains(e, "AWS_ACCESS_KEY_ID=") ||
+			strings.Contains(e, "AWS_SECRET_ACCESS_KEY=") ||
+			strings.Contains(e, "AWS_SESSION_TOKEN=") {
+			continue
+		}
+
+		newEnv = append(newEnv, e)
+	}
+
+	newEnv = append(newEnv,
+		"AWS_ACCESS_KEY_ID="+accessKeyID,
+		"AWS_SECRET_ACCESS_KEY="+secretAccessKey,
+		"AWS_SESSION_TOKEN="+sessionToken,
+		"AWS_REGION="+"us-west-2")
+
+	return newEnv
+}
