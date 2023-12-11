8eec08b155cc614104581501c57afd589090c9fc
Danny Schofield <dannyschofield28@gmail.com>
2023-12-11 15:28:58 -0500

    lib ymlparser: include Azure diff for resourceoperation
    Diff the current subscriptions with what currently exists to determine
    if a new Azure subscription needs to be created.
    
    Break out the ResourceOperation into its own file

---
+++ lib/ymlparser/interface.go
@@ -0,0 +1,14 @@
+package ymlparser
+
+import (
+	"context"
+
+	"github.com/santiago-labs/telophasecli/lib/awsorgs"
+)
+
+type ResourceOperation interface {
+	Call(context.Context, awsorgs.Client) error
+	ToString() string
+	AddDependent(ResourceOperation)
+	ListDependents() []ResourceOperation
+}
