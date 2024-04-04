e8dff5ac08cd7b2e78867dbfa98c69f40cee6a1e
Dylan Kapoor <dylankapoor@gmail.com>
2024-04-04 13:20:59 -0400

    adding support for linux to setup script

--- setup.sh
+++ setup.sh
@@ -10,12 +10,21 @@ print_green() {
     echo -e "\033[0;32m$1\033[0m"
 }
 
-if command -v brew &> /dev/null
-then
+if command -v brew &> /dev/null; then
     echo "Brew is installed, proceeding with awscli and terraform installation."
     brew install awscli terraform
+elif [ "$(uname)" = "Linux" ]; then
+    if [ -f /etc/debian_version ]; then
+        echo "Debian-based Linux detected. Installing packages with apt."
+        sudo apt update && sudo apt install -y awscli
+        sudo snap install terraform --classic
+    elif [ -f /etc/redhat-release ]; then
+        echo "Red Hat-based Linux detected. Installing packages with dnf."
+        sudo dnf install -y awscli
+        print_red "Please manually install Terraform following the official instructions."
+    fi
 else
-    print_red "Brew is not installed please install awscli and terraform manually"
+    print_red "Brew is not installed, or you're not on a supported Linux distribution. Please install awscli and terraform manually."
     exit 1
 fi
 
@@ -24,7 +33,7 @@ then
     echo "Installing aws-cdk and aws-cdk-local."
     npm install -g aws-cdk aws-cdk-local
 else
-    print_red "npm is not installed, please install it and then return"
+    print_red "npm is not installed. Please install it and then return."
     exit 1
 fi
 
@@ -33,7 +42,13 @@ then
     echo "Installing telophasecli..."
     go install github.com/santiago-labs/telophasecli@latest
 else
-    print_red "go is not installed. Please install the language then return."
+    if [ "$(uname)" = "Darwin" ]; then
+        print_red "Go is not installed. On Mac, you can install Go using Homebrew with: brew install go. Then, return to run this script again."
+    elif [ "$(uname)" = "Linux" ]; then
+        print_red "Go is not installed. On Linux, you can generally install Go by running: sudo snap install go --classic (Debian/Ubuntu) or sudo dnf install golang (Fedora/RHEL). Then, return to run this script again."
+    else
+        print_red "Go is not installed. Please visit https://golang.org/doc/install for instructions on how to install Go on your system."
+    fi
     exit 1
 fi
 
