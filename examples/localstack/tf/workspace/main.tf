ceb4e0ce9e2d8bda48c85edb4121016b536293db
Danny Schofield <dannyschofield28@gmail.com>
2024-04-19 11:55:19 -0400

    examples localstack: include workspaces setup

---
+++ examples/localstack/tf/workspace/main.tf
@@ -0,0 +1,30 @@
+resource "aws_dynamodb_table" "example" {
+  name             = "${terraform.workspace}-eu"
+
+  hash_key         = "TestTableHashKey"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "TestTableHashKey"
+    type = "S"
+  }
+}
+
+locals {
+  region = split("_",terraform.workspace)[1]
+}
+
+provider "aws" {
+    # Two options can use ${telophase.region} or look at local config
+    region = local.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-${telophase.account_id}"
+    key    = "workspace/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
